ba0a5932e476d8789e711ce091bf41b368d0b69a
Towhid47 <mdtowhidurrahman47@gmail.com>
2024-01-21 23:01:49 +0600

    Footer Added

---
+++ src/components/layouts/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='p-5 text-white bg-slate-800'>
+             This is Footer
+        </div>
+    );
+};
+
+export default Footer;
